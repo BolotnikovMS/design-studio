ddaa36573394835216cc96db6c0a7127b6e51170
BolotnikovMS <bolotnikovms@yandex.ru>
2022-07-24 21:52:15 +0500

    Update

--- js/main.js
+++ js/main.js
@@ -5,12 +5,26 @@ window.addEventListener('DOMContentLoaded', () => {
     duration: 1500,
   })
 
-  $("a[href*='#']").on("click", function(e){
-    const anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $(anchor.attr('href')).offset().top
-    }, 777);
-    e.preventDefault();
-    return false;
-  });
+  // Links scroll
+  const headerNav = document.querySelector('.header-nav')
+  const footerNav = document.querySelector('.footer-nav')
+
+  function linkScroll(nav) {
+    const links = nav.querySelectorAll('a[href*="#"')
+
+    $(links).on("click", function(e){
+      const anchor = $(this)
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 777);
+      e.preventDefault()
+      return false;
+    })
+  }
+
+  linkScroll(headerNav)
+  linkScroll(footerNav)
+
+  // Nav fixed
+  
 })
