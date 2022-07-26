1e30c3f559ec1cc65a7ec824d6badf7023f53fcd
BolotnikovMS <bolotnikovms@yandex.ru>
2022-07-26 22:40:51 +0500

    Update

--- js/main.js
+++ js/main.js
@@ -5,6 +5,25 @@ window.addEventListener('DOMContentLoaded', () => {
     duration: 1500,
   })
 
+  // Typing text
+  const homeTitleText = `We're a multi-disciplinary design studio that helps brands thrive.` // Ваш текст
+	const delay = 90; // Скорость печатания
+	const homeTitle = document.querySelector('.home__title'); // id элемента для вывода результата
+
+  const typedText = (text, elem, delay) => {
+    if(text.length > 0) {
+			elem.innerHTML += text[0]
+      
+			setTimeout(
+				function() {
+					typedText(text.slice(1), elem, delay); 
+				}, delay
+			)
+		}
+  }
+
+	typedText(homeTitleText, homeTitle, delay);
+
   // Links scroll
   const headerNav = document.querySelector('.header-nav')
   const footerNav = document.querySelector('.footer-nav')
@@ -25,6 +44,24 @@ window.addEventListener('DOMContentLoaded', () => {
   linkScroll(headerNav)
   linkScroll(footerNav)
 
-  // Nav fixed
-  
+  // Fixing navigation on scroll  
+  const headerFixed = () => {
+    const header = document.querySelector('.header')
+    const home = document.querySelector('.home')  
+    let scrollTop = window.scrollY
+    let homeCenter = home.offsetHeight / 2
+
+    if (scrollTop >= homeCenter) {
+      header.classList.add('header-fixed')
+      home.style.marginTop = `${header.offsetHeight}px`
+    } else {
+      header.classList.remove('header-fixed')
+      home.style.marginTop = `0px`
+    }
+  }
+
+  headerFixed()
+  window.addEventListener('scroll', () => {
+    headerFixed()
+  })
 })
